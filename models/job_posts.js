aab02b4a9b065f36cf2c2ad6a676be9184be4b97
William Barbee <willbarbee@sbcglobal.net>
2015-04-01 10:32:23 -0400

    timestamps redux

--- models/job_posts.js
+++ models/job_posts.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
       employer_id: DataTypes.INTEGER,
 }, {
 
-  timestamps: false,
+  timestamps: true,
 
   classMethods: {
       associate: function(models) {
