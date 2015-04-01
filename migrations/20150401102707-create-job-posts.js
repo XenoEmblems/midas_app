240b90dcfd30b4d062faab1800c785d56f08c25b
William Barbee <willbarbee@sbcglobal.net>
2015-04-01 10:30:12 -0400

    midas job posts model and migration

---
+++ migrations/20150401102707-create-job-posts.js
@@ -0,0 +1,57 @@
+"use strict";
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.createTable("job_posts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      job_title: {
+        DataTypes.STRING,
+      },
+      position: {
+        DataTypes.STRING,
+      },
+      post_url: {
+        DataTypes.STRING,
+      },
+      post_content: {
+        DataTypes.TEXT,
+      },
+      job_ID: {
+        DataTypes.STRING,
+      },
+      employer_name: {
+        DataTypes.STRING,
+      },
+      salary: {
+        DataTypes.STRING,
+      },
+      date_posted: {
+        DataTypes.STRING,
+      },
+      date_created: {
+        DataTypes.STRING,
+      },
+      post_id: {
+        DataTypes.INTEGER,
+      },
+      location: {
+        DataTypes.STRING,
+      },
+      is_entry: {
+        DataTypes.BOOLEAN,
+      },
+      employer_id: {
+        DataTypes.INTEGER,
+      },
+    }).done(done);
+  },
+
+  down: function(migration, DataTypes, done) {
+    migration.dropTable("job_posts").done(done)
+  }
+};
