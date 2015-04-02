e8f375a439dadc16e88c7ceef0ab08da713b7c7b
William Barbee <willbarbee@sbcglobal.net>
2015-04-02 15:00:00 -0400

    merge request resolved

--- public/js/views/jobPostListView.js
+++ public/js/views/jobPostListView.js
@@ -12,5 +12,20 @@ App.Views.JobPostListView = Backbone.View.extend ({
 
     renderOne: function() {
       this.$el.append(new App.Views.JobPostModalView({ model: jobPost }).$el);
-    }
+    },
+
+    events: {
+  'click .show-more': 'showModal'
+    },
+
+    showModal: function(event) {
+      var model = this.collection.get( $(event.target).data('id') );
+      model.fetch({
+        success: function(model) {
+          console.log(model);
+          new App.Views.JobPostModalView({ model: model });
+        }
+  });
+}
+});
 });
