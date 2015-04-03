3f76171f9e0244c261707030404771dbbec7eea8
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2015-04-03 00:54:41 -0400

    not yet...

--- public/js/collections/jobPosts.js
+++ public/js/collections/jobPosts.js
@@ -1,4 +1,7 @@
 App.Collections.JobPosts = Backbone.Collection.extend ({
+    initialize: function() {
+    	console.log("JobPosts collection created");
+    },
     url: '/job_posts',
     model: App.Models.JobPost
 });
--- public/js/views/jobPostListView.js
+++ public/js/views/jobPostListView.js
@@ -2,16 +2,25 @@ App.Views.JobPostList = Backbone.View.extend ({
 
       el: '#main',
 
-      intitialize: function() {
+      initialize: function() {
+        console.log("new JobPostList view created");
+        console.log(this.collection);
         this.template = Handlebars.compile( $('#list-view-template').html() );
         this.listenTo(this.collection, 'reset', this.renderAll() );
+        this.collection.fetch();
       },
 
       renderAll: function() {
-        this.collection.each(this.renderOne, this);
+        console.log("renderAll called")
+        console.log(this.collection.length); // this isn't getting reset...
+        this.collection.each(function (one) {
+          console.log("heyyyyyy");
+        })
       },
 
       renderOne: function(model) {
+        console.log(model);
+        this.$el.append("RENDER ONE CALLED");
         var previewTemplate = Handlebars.compile($('#list-preview-template').html());
         this.$el.append(previewTemplate(model.toJSON()));
       },
