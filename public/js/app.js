5d484071eb6aed6883b2164c1e55421f4b8f12d5
Eric Doyle <edoyle@drew.edu>
2015-04-04 09:15:01 -0400

    cleaning

--- public/js/app.js
+++ public/js/app.js
@@ -9,4 +9,8 @@ $(function() {
 	console.log('Loaded, bro.');
 	// the router takes care of the rest of setup
 	App.router = new App.Routers.JobPostRouter();
+
+  //App.jobPosts = new App.Collections.Jobposts;
+  //App.jobsView = new App.Views.JobPostList({ collection: App.jobPosts });
+
 });
