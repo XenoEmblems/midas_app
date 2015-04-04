d73d1485d859f258ba43c44c0335825037e6d108
William Barbee <willbarbee@sbcglobal.net>
2015-04-03 22:19:48 -0400

    stuff

--- public/js/models/jobPost.js
+++ public/js/models/jobPost.js
@@ -1,10 +1,10 @@
 App.Models.JobPost = Backbone.Model.extend({
-  //urlRoot: '/job_post',
+  urlRoot: '/job_post',
 
-  //parse: function(data) {
+  // parse: function(data) {
   //  if (data.job_posts) {
   //    this.members = new App.Collections.JobPosts();
   //    this.members.add(data.job_posts);
   //  }
-  //}
+  // }
 });
