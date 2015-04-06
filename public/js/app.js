5b4dd31cd214ad03996569d1fe565e6ebabec2a8
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2015-04-06 00:45:19 -0400

    form starting to happen

--- public/js/app.js
+++ public/js/app.js
@@ -5,20 +5,19 @@ var App = {
 	Routers: {}
 };
 
-function cookieTest(){
-	var cookies = document.cookie
-	var userName = 'Mark R. Zuckerberg'
-	console.log("do I have cookies?", cookies)
-	document.cookie = 'name: ' + userName;
-	var cookies = document.cookie
-	console.log("do I have one now?", cookies)
-
-}
+//function cookieTest(){
+//	var cookies = document.cookie
+//	var userName = 'Mark R. Zuckerberg'
+//	console.log("do I have cookies?", cookies)
+//	document.cookie = 'name: ' + userName;
+//	var cookies = document.cookie
+//	console.log("do I have one now?", cookies)
+//
+//}
 
 
 $(function() {
 	console.log('Loaded, bro.');
 	// the router takes care of the rest of setup
-	cookieTest();
 	App.jobsRouter = new App.Routers.JobPostRouter();
 });
