485517c04b7fddbbb08ffa0c311ed9fa06674cd2
Eric Doyle <edoyle@drew.edu>
2015-04-06 11:54:25 -0400

    HTML Replace

--- models/job_posts.js
+++ models/job_posts.js
@@ -1,10 +1,13 @@
 "use strict";
+
+
+
 module.exports = function(sequelize, DataTypes) {
   var job_posts = sequelize.define('job_posts', {
       job_title: DataTypes.STRING,
       position_id: DataTypes.INTEGER,
       post_url: DataTypes.STRING,
-      post_content: DataTypes.TEXT,
+      post_content: DataTypes.STRING,
       job_ID: DataTypes.INTEGER,
       employer_name: DataTypes.STRING,
       salary: DataTypes.STRING,
