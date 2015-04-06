714181603228da3f7492910bbe622eb7792f47ec
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2015-04-06 14:56:21 -0400

    button working

---
+++ public/js/views/jobOps.js
@@ -0,0 +1,14 @@
+App.Views.JobOps = Backbone.View.extend({
+	el: '#job-ops',
+
+	events: {
+		'click .show-me-jobs': 'showJobs'
+	},
+
+	showJobs: function(){
+		$('.show-me-jobs').remove();
+		this.$el.append(App.jobsView.$el);
+
+
+	}
+})
